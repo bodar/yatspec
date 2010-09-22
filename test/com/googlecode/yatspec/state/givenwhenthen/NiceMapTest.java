ef2fed30b1270f80158f67182fbc6809a0aa96e1
matthew.savage <matthew.savage@localhost>
2010-09-22 14:53:22 +0100

    matt, alex: writing some actual unit tests before comitting code so that it, y'know, works and stuff

---
+++ test/com/googlecode/yatspec/state/givenwhenthen/NiceMapTest.java
@@ -0,0 +1,22 @@
+package com.googlecode.yatspec.state.givenwhenthen;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class NiceMapTest {
+    @Test
+    public void shouldGiveAUsefulMessageIfTheWrongClassIsRequested() throws Exception {
+        NiceMap map = new NiceMap("Moomin");
+        try {
+            map.getType("String", Integer.class);
+            fail("Expected exception");
+        } catch (Exception e) {
+            assertThat(e.getMessage(), containsString("Moomin"));
+        }
+    }
+
+}
+
