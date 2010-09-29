9b5011f559e41185b4de43bcdb90e745e301ba8b
Daniel Worthington-Bodart <dan@bodar.com>
2010-09-29 22:06:05 +0100

    Moved Regex support to totallylazy

--- src/com/googlecode/yatspec/parsing/Text.java
+++ src/com/googlecode/yatspec/parsing/Text.java
@@ -1,27 +1,31 @@
 package com.googlecode.yatspec.parsing;
 
 import com.googlecode.totallylazy.Callable1;
-import com.googlecode.yatspec.regex.Matches;
+import com.googlecode.totallylazy.regex.Matches;
+import com.googlecode.totallylazy.regex.Regex;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
+import static com.googlecode.totallylazy.regex.Matches.filterNull;
+import static com.googlecode.totallylazy.regex.Regex.regex;
+
 public class Text {
-    private static final Pattern wordDelimiter = Pattern.compile("[\\\\(\\\\),\\\\.;]|([A-Z]*?)([A-Z][a-z])");
+    private static final Regex wordDelimiter = Regex.regex("[\\\\(\\\\),\\\\.;]|([A-Z]*?)([A-Z][a-z])");
     private static final Pattern spaceRemover = Pattern.compile("\\s*(\\S+\\s)");
-    private static final Pattern stringIgnorer = Pattern.compile("\"[^\"]+\"");
+    private static final Regex stringIgnorer = regex("\"[^\"]+\"");
 
     private static final Callable1<MatchResult, String> wordDelimiterReplacer = new Callable1<MatchResult, String>() {
         public String call(MatchResult matchResult) {
             // " $1 $2"
-            return " " + lowercaseSingleLetters(Matches.filterNull(matchResult.group(1))) + " " + Matches.filterNull(matchResult.group(2)).toLowerCase();
+            return " " + lowercaseSingleLetters(filterNull(matchResult.group(1))) + " " + filterNull(matchResult.group(2)).toLowerCase();
         }
     };
 
     private static final Callable1<String, String> wordifier = new Callable1<String, String>() {
         public String call(String text) {
-            return Matches.matches(wordDelimiter, text).replace(wordDelimiterReplacer);
+            return wordDelimiter.matches(text).replace(wordDelimiterReplacer);
         }
     };
 
@@ -42,8 +46,7 @@ public class Text {
 
 
     public static String wordify(String value) {
-        final String wordified = Matches.matches(stringIgnorer, value).replace(wordifier, doNothing);
+        final String wordified = stringIgnorer.matches(value).replace(wordifier, doNothing);
         return StringUtils.capitalize(spaceRemover.matcher(wordified).replaceAll(spaceRemoverReplacer));
     }
-   
 }
