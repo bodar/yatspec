88e61bb1c2c4126f15eddaacc5668946fc682e14
Daniel Worthington-Bodart <dan@bodar.com>
2010-10-12 14:17:40 +0100

    Upgraded to totallylazy version 80

--- src/com/googlecode/yatspec/parsing/Command.java
+++ src/com/googlecode/yatspec/parsing/Command.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class Command {
     public static String exec(String ... args) throws Exception {
-        final Process process = Runtime.getRuntime().exec(args);
+        final Process process = new ProcessBuilder(args).start();
         process.waitFor();
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         return reader.readLine();
