43b84cff029688ea4a02331053f550cd6ee532ed
Matthew Savage <matthew.savage@gmail.com>
2011-01-21 16:52:23 +0000

    Allowed yatspec to output parsed contents of tests in abstract test case

--- src/com/googlecode/yatspec/parsing/TestParser.java
+++ src/com/googlecode/yatspec/parsing/TestParser.java
@@ -1,6 +1,5 @@
 package com.googlecode.yatspec.parsing;
 
-import com.googlecode.totallylazy.Pair;
 import com.googlecode.totallylazy.Predicate;
 import com.googlecode.totallylazy.Sequence;
 import com.googlecode.totallylazy.Strings;
@@ -16,22 +15,36 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 import static com.googlecode.totallylazy.Predicates.notNull;
+import static com.googlecode.totallylazy.Sequences.empty;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.yatspec.parsing.TestMethodExtractor.extractTestMethod;
+import static java.util.Collections.emptyList;
 
 public class TestParser {
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
     public static List<TestMethod> parseTestMethods(Class aClass) throws Exception {
+        final Sequence<Method> methods = getMethods(aClass);
+
+        return collectTestMethods(aClass, methods).toList();
+    }
+
+    private static Sequence<TestMethod> collectTestMethods(Class aClass, Sequence<Method> methods) throws JaxenException, IOException {
+        if (!canGetJavaSourceFile(aClass)) {
+            return empty();
+        }
         final File javaSource = getJavaSourceFile(aClass);
+
         final InputStream stream = new FileInputStream(javaSource);
         final String wholeFile = Strings.toString(stream);
 
         final ASTCompilationUnit classAST = getClassAST(wholeFile);
         final Sequence<ASTMethodDeclaration> methodASTs = getMethodAST(classAST);
-        final Sequence<Method> methods = getMethods(aClass);
 
-        return methodASTs.zip(methods).map(extractTestMethod(wholeFile)).filter(notNull(TestMethod.class)).toList();
+        Sequence<TestMethod> myTestMethods = methodASTs.zip(methods).map(extractTestMethod(wholeFile)).filter(notNull(TestMethod.class));
+        Sequence<TestMethod> parentTestMethods = collectTestMethods(aClass.getSuperclass(), methods);
+
+        return myTestMethods.join(parentTestMethods);
     }
 
     private static ASTCompilationUnit getClassAST(String wholeFile) {
@@ -59,9 +72,19 @@ public class TestParser {
     }
 
     private static File getJavaSourceFile(Class testClass) {
-        final String filename = getPath(testClass.getSimpleName()) + ".java";
+        final String filename = javaSourceFilename(testClass);
         return Files.find(filename);
     }
+
+    private static boolean canGetJavaSourceFile(Class testClass) {
+        final String filename = javaSourceFilename(testClass);
+        return Files.canFind(filename);
+    }
+
+    private static String javaSourceFilename(Class testClass) {
+        return getPath(testClass.getSimpleName()) + ".java";
+    }
+
 
     public static String getPath(String testClassName) {
         return testClassName.replaceAll("\\.", "/");
