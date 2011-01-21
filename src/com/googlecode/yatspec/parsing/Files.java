2a941fc54de6f44ff902a06251ee2f8d3685ef1b
Matthew Savage <matthew.savage@gmail.com>
2011-01-21 16:52:23 +0000

    Allowed yatspec to output parsed contents of tests in abstract test case

--- src/com/googlecode/yatspec/parsing/Files.java
+++ src/com/googlecode/yatspec/parsing/Files.java
@@ -1,18 +1,27 @@
 package com.googlecode.yatspec.parsing;
 
-import com.googlecode.yatspec.parsing.Command;
-
 import java.io.File;
 
 public class Files {
     public static File find(String filename) {
         try {
-            final String workingDirectory = System.getProperty("user.dir");
-            final String absolutePath = Command.exec("find", workingDirectory, "-name", filename);
+            final String absolutePath = executeFindCommand(filename);
             return new File(absolutePath);
         } catch (Exception e) {
             System.err.println("Couldn't find file " + filename);
             throw new RuntimeException(e);
         }
+    }
+    public static boolean canFind(String filename) {
+        try {
+            return executeFindCommand(filename) != null;
+        } catch (Exception ignore) {
+            return false;
+        }
+    }
+
+    private static String executeFindCommand(String filename) throws Exception {
+        final String workingDirectory = System.getProperty("user.dir");
+        return Command.exec("find", workingDirectory, "-name", filename);
     }
 }
