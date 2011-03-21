6a394291d6d9e3903128b3f169d29683cf3c3b96
Matt Savage <matthew.savage@gmail.com>
2011-03-21 17:21:50 +0000

    Removed duplication from totallylazy

--- test/com/googlecode/yatspec/parsing/FilesTest.java
+++ test/com/googlecode/yatspec/parsing/FilesTest.java
@@ -1,24 +1,28 @@
 package com.googlecode.yatspec.parsing;
 
+import com.googlecode.totallylazy.Option;
 import org.junit.Test;
 
 import java.io.File;
 
-import static com.googlecode.yatspec.parsing.Predicates.pathEndsWith;
+import static com.googlecode.totallylazy.Files.name;
+import static com.googlecode.totallylazy.Option.none;
+import static com.googlecode.totallylazy.Predicates.where;
+import static com.googlecode.totallylazy.Strings.endsWith;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FilesTest {
 
     @Test
     public void shouldFindFile() {
-        assertThat(Files.findOnly(workingDirectory(), pathEndsWith("FilesTest.java")), notNullValue());
+        assertThat(Files.find(workingDirectory(), where(name(), endsWith("FilesTest.java"))), notNullValue());
     }
 
     @Test
     public void shouldNotFindFile() {
-        assertThat(Files.findOnly(workingDirectory(), pathEndsWith("doesNotExist")), nullValue());
+        assertThat(Files.find(workingDirectory(), where(name(), endsWith("doesNotExist"))), is((Option<File>)none(File.class)));
     }
 
     private File workingDirectory() {
