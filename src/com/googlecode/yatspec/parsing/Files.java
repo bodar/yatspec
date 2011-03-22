6d81f6f5af30f6170922480baeee0e57b72646b6
Daniel Worthington-Bodart <dan@bodar.com>
2011-03-22 07:10:02 +0000

    Updated to latest TL to verify recursiveFiles works as expected

--- src/com/googlecode/yatspec/parsing/Files.java
+++ src/com/googlecode/yatspec/parsing/Files.java
@@ -1,14 +1,5 @@
 package com.googlecode.yatspec.parsing;
 
-import com.googlecode.totallylazy.*;
-
-import java.io.File;
-
-import static com.googlecode.totallylazy.Files.files;
-import static com.googlecode.totallylazy.Files.isDirectory;
-import static com.googlecode.totallylazy.Files.recursiveFiles;
-
-@SuppressWarnings("unchecked")
 public class Files {
 
     public static String toJavaPath(Class testClass) {
@@ -19,15 +10,7 @@ public class Files {
         return toPath(testClass) + ".html";
     }
 
-    public static Option<File> find(File directory, Predicate<? super File> filePredicate) {
-        return recursiveFiles(directory).filter(filePredicate).headOption();
-    }
-
-    public static Sequence<File> filter(File directory, final Predicate<? super File> filePredicate) {
-        return recursiveFiles(directory).filter(filePredicate);
-    }
-
-    private static String toPath(Class clazz) {
+    public static String toPath(Class clazz) {
         return clazz.getName().replaceAll("\\.", "/");
     }
 }
