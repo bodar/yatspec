498b1752fcf591194a97dc4548cfabd9e9de1840
Daniel Worthington-Bodart <dan@bodar.com>
2011-05-04 11:15:45 +0100

    Tidied renderer tests

--- test/com/googlecode/yatspec/rendering/EnhancedStringTemplateGroupTest.java
+++ test/com/googlecode/yatspec/rendering/EnhancedStringTemplateGroupTest.java
@@ -1,37 +1,32 @@
 package com.googlecode.yatspec.rendering;
 
-import org.junit.Before;
+import org.antlr.stringtemplate.AttributeRenderer;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+import static com.googlecode.yatspec.rendering.Annotations.notesProxyClass;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class EnhancedStringTemplateGroupTest {
-    private static final String VALUE = "A";
-    private static final String OTHER_VALUE = "B";
     private EnhancedStringTemplateGroup stringTemplateGroup = new EnhancedStringTemplateGroup("name");
-
-    @Before
-    public void setUp() {
-        stringTemplateGroup.registerDefaultRenderer(new DelegateAttributeRenderer(VALUE));
-    }
 
     @Test
     public void shouldReturnDefaultRendererIfNoneFound() {
-        assertThat(stringTemplateGroup.getAttributeRenderer(String.class).toString(null), equalTo(VALUE));
+        AttributeRenderer defaultRenderer = new XmlStringRenderer();
+        stringTemplateGroup.registerDefaultRenderer(defaultRenderer);
+        assertThat(stringTemplateGroup.getAttributeRenderer(String.class), sameInstance(defaultRenderer));
     }
 
     @Test
-    public void shouldReturnClassRenderer() {
-        stringTemplateGroup.registerRenderer(String.class, new DelegateAttributeRenderer(OTHER_VALUE));
-
-        assertThat(stringTemplateGroup.getAttributeRenderer(String.class).toString(null), equalTo(OTHER_VALUE));
+    public void shouldPreserveDefaultBehaviour() {
+        AttributeRenderer stubRenderer = new XmlStringRenderer();
+        stringTemplateGroup.registerRenderer(String.class, stubRenderer);
+        assertThat(stringTemplateGroup.getAttributeRenderer(String.class), sameInstance(stubRenderer));
     }
 
     @Test
-    public void shouldReturnAnnotationRenderer() throws NoSuchMethodException {
-        stringTemplateGroup.registerRenderer(Test.class, new DelegateAttributeRenderer(OTHER_VALUE));
-
-        assertThat(stringTemplateGroup.getAttributeRenderer(Test.class).toString(null), equalTo(OTHER_VALUE));
+    public void shouldReturnAnnotationRenderer() throws Exception {
+        assertThat(stringTemplateGroup.getAttributeRenderer(notesProxyClass()), instanceOf(AnnotationRenderer.class));
     }
 }
