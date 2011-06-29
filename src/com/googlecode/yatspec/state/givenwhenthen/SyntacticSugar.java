b0ffce7944decf39ff869cccdf2c02e0d82ea0c2
Alex Luker <alex.luker@gmail.com>
2011-06-29 23:58:54 +0100

    Syntactic sugar methods for readable documentation

---
+++ src/com/googlecode/yatspec/state/givenwhenthen/SyntacticSugar.java
@@ -0,0 +1,16 @@
+package com.googlecode.yatspec.state.givenwhenthen;
+
+public class SyntacticSugar {
+
+    public static <T> T of(T t) {
+        return t;
+    }
+
+    public static <T> T to(T t) {
+        return t;
+    }
+
+    public static <T> T and(T t) {
+        return t;
+    }
+}
