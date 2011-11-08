c840a91700a72889cca7855f67a25e30234a5742
Steve Smith <steve@stephen-smith.co.uk>
2011-11-08 17:47:36 +0000

    Look for Java source file in classpath before file system

--- test/com/googlecode/yatspec/parsing/TestParserTest.java
+++ test/com/googlecode/yatspec/parsing/TestParserTest.java
@@ -12,8 +12,11 @@ import java.util.List;
 
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.yatspec.parsing.TestParser.parseTestMethods;
+import static java.lang.String.format;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpecRunner.class)
 public class TestParserTest {
@@ -33,6 +36,19 @@ public class TestParserTest {
     })
     public void yatspecWillTrimWhitespaceLeftBehindByQDoxInTableTestAnnotationsWhenAFieldVariableIsDeclared(String something) throws Exception {
         //A workaround for weirdness in QDox
+    }
+
+    @Test
+    public void shouldParseTestMethodsFromClassFoundInClassPathRatherThanFileSystem() throws Exception {
+        assertExistsInClassLoader("com/googlecode/yatspec/parsing/test/TestSource.java", "TestParserTest-sources.jar");
+        assertExistsInClassLoader("com/googlecode/yatspec/parsing/test/TestSource.class", "TestParserTest.jar");
+
+        List<TestMethod> testMethods = parseTestMethods(Class.forName("com.googlecode.yatspec.parsing.test.TestSource"));
+        assertThat(sequence(testMethods).first().getName(), equalTo("testParseMethodWithSrcJar"));
+    }
+
+    private static void assertExistsInClassLoader(String resource, String location) {
+        assertNotNull(format("Resource '%s' not found, check location '%s' is in classpath?", resource, location), Thread.currentThread().getContextClassLoader().getResource(resource));
     }
 
 }
