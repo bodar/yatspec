252c7bda61ac4643a3fff416c2b9b773137d60a6
Alex Luker <alex.luker@gmail.com>
2012-04-04 14:27:51 +0100

    alex.luker

--- test/com/googlecode/yatspec/parsing/TextTest.java
+++ test/com/googlecode/yatspec/parsing/TextTest.java
@@ -1,5 +1,6 @@
 package com.googlecode.yatspec.parsing;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,7 +20,14 @@ public class TextTest {
     }
 
     @Test
-    public void wordifyShouldNotRemoveLeadingSpaces() {
-         assertThat(Text.wordify("Foo\nBar"), is("Foo\nBar"));
+    public void insertsSpaceOnFullStops() throws Exception {
+        assertThat(Text.wordify("something.and(then.perhaps, something.else)"), is("Something and then perhaps something else"));
+    }
+
+    @Test
+    public void doesNotInsertSpaceOrRemoveDecimalPointForFloats() throws Exception {
+        assertThat(Text.wordify("assertThat(sqrt(421.0704), is(20.520));"), is("Assert that sqrt 421.0704 is 20.520"));
+        assertThat(Text.wordify("13.37 66.6 3.33"), is("13.37 66.6 3.33"));
+        assertThat(Text.wordify("someMethod(12.5,99, 22.22"), is("Some method 12.5 99 22.22"));
     }
 }
