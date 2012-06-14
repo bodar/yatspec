82555396e1ed153c8aaaffab020847d7c981c243
alex luker <alex.luker@gmail.com>
2012-06-14 18:23:34 +0100

    Fixing Issue-15 by escaping Java Source

--- src/com/googlecode/yatspec/rendering/html/JavaSourceRenderer.java
+++ src/com/googlecode/yatspec/rendering/html/JavaSourceRenderer.java
@@ -2,19 +2,20 @@ package com.googlecode.yatspec.rendering.html;
 
 import com.googlecode.totallylazy.Sequence;
 import com.googlecode.yatspec.parsing.JavaSource;
-import com.googlecode.yatspec.parsing.Text;
 import com.googlecode.yatspec.rendering.Renderer;
 
 import java.util.regex.Pattern;
 
 import static com.googlecode.totallylazy.Sequences.sequence;
+import static com.googlecode.totallylazy.Xml.escape;
+import static com.googlecode.yatspec.parsing.Text.wordify;
 
 public class JavaSourceRenderer implements Renderer<JavaSource> {
     private static final Pattern DOT_CLASS = Pattern.compile("\\.class(\\W|$)");
 
     @Override
     public String render(JavaSource javaSource) throws Exception {
-        return lines(removateDotClass(javaSource.value().trim())).map(Text.wordify()).toString("\n");
+        return escape(lines(removateDotClass(javaSource.value().trim())).map(wordify()).toString("\n"));
     }
 
     public static Sequence<String> lines(final String sourceCode) {
